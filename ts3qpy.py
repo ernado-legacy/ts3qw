2c43cbac52e1a257cd18a45fd1b6d5d43928777e
Ernado <sertav.ernado@gmail.com>
2012-12-14 00:56:42 +0400

    added first version added first test

---
+++ ts3qpy.py
@@ -0,0 +1,72 @@
+# coding=utf8
+import getpass
+import sys
+import telnetlib
+import time
+import logging
+
+
+class ConnectionFailed(Exception):
+    pass
+
+
+class QueryClient:
+    """
+    Teamspeak 3 Query client
+    """
+
+    def __init__(self, host, port=10011):
+        """
+        :host: trollo
+        :type host: str
+        :type port: int
+        """
+        self.host = host
+        self.port = port
+        self.timeout = 5
+        self.tn = None
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.addHandler(ch)
+        self.connect()
+
+    def connect(self):
+        self.logger.debug('Connecting to %s:%s' % (self.host, self.port))
+        self.tn = telnetlib.Telnet(self.host, self.port, self.timeout)
+        self.logger.debug('Receiving message')
+        reply = self.tn.read_until('TS', self.timeout)
+        if reply != 'TS':
+            self.logger.error('Server is not Teamspeak 3 server')
+            raise ConnectionFailed
+        self.logger.debug('Connected')
+
+    def disconnect(self):
+        if self.tn:
+            self.tn.close()
+            self.tn = None
+
+
+HOST = "cygame.ru"
+PORT = 10011
+TIMEOUT = 5
+#user = raw_input("Enter your remote account: ")
+#password = getpass.getpass()
+
+#tn = telnetlib.Telnet(HOST, port=10011, timeout=TIMEOUT)
+#o = tn.read_until('TS3', TIMEOUT)
+#print o
+#tn.write('help\n')
+#print tn.read_until('msg=ok', TIMEOUT)
+if __name__ == '__main__':
+    q = QueryClient('cygame.ru')
+#
+#if password:
+#    tn.read_until("Password: ")
+#    tn.write(password + "\n")
+#
+#tn.write("ls\n")
+#tn.write("exit\n")
