7a403837b3de7149e3013e8656173e2d6188b48b
Ernado <sertav.ernado@gmail.com>
2012-12-14 01:27:32 +0400

    added settings

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,21 @@
 import unittest
 
+HOST = '127.0.0.1'
+PORT = 10011
+TOKEN = ''
+
+try:
+    from local import *
+except ImportError:
+    pass
+
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
+
+class InitCase(unittest.TestCase):
+    pass
 
 if __name__ == '__main__':
     unittest.main()
