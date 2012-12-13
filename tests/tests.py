9826ff47abd3ff8c9703a4c1ae8fcf99fe973ce3
Alexander Razumov <sertav.ernado@gmail.com>
2012-12-14 02:34:54 +0400

    added initial test

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,10 @@
 import unittest
+import ts3qpy
 
 HOST = '127.0.0.1'
 PORT = 10011
 TOKEN = ''
+
 
 try:
     from local import *
@@ -14,8 +16,16 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
 
+
 class InitCase(unittest.TestCase):
-    pass
+    def test_init(self):
+        self.q.connect()
+
+    def setUp(self):
+        self.q = ts3qpy.QueryClient(HOST, PORT)
+
+    def tearDown(self):
+        self.q.disconnect()
 
 if __name__ == '__main__':
     unittest.main()
