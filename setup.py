444ad8e62b535b9ceed9d7a8f6a32fea7b3b1deb
Ernado <sertav.ernado@gmail.com>
2012-12-15 20:05:49 +0400

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ts3qw',
+    version='0.01',
+    packages=['tests'],
+    url='http://cygame.ru',
+    license='Apache',
+    author='Ernado',
+    author_email='',
+    description='Teamspeak 3 Server Query Wrapper'
+)
--- tests/tests.py
+++ tests/tests.py
@@ -14,6 +14,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
 
+
 class InitCase(unittest.TestCase):
     pass
 
