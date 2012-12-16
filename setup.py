aedfd3dabf1480f246bd40354998f705c85bc397
Ernado <sertav.ernado@gmail.com>
2012-12-16 04:22:03 +0400

    fixed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ts3qw',
     version='0.01',
-    packages=['tests'],
+    packages=['ts3qw'],
     url='http://cygame.ru',
     license='Apache',
     author='Ernado',
